f182d09d4eef8b396fc8ae0abedb63ed96e7a574
val17102 <val17102@uvg.edu.gt>
2021-01-30 14:29:09 -0600

    Agregando animaciones a CM y Agregando Melee

---
+++ GGJ21/Assets/Scripts/CM_Melee.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM_Melee : MonoBehaviour
+{
+    public float attackRange = 0.5f;
+    // Start is called before the first frame update
+    public void Attack(Transform attackPoint){
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange);
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Hit" + enemy.name);
+        }
+    }
+}
