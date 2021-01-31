6df6f1cb7a0128d7296d4ebbdb472277d713aa65
josepablito16 <josepablocif16@gmail.com>
2021-01-30 19:43:12 -0600

    Player and enemy join

--- GGJ21/Assets/Scripts/E_EnemyController.cs
+++ GGJ21/Assets/Scripts/E_EnemyController.cs
@@ -92,7 +92,7 @@ public class E_EnemyController : MonoBehaviour
         yield return new WaitForSeconds(Random.Range(2f, 8f));
         randomDirecton = new Vector3(0,0, Random.Range(0, 360));
         Quaternion nextRotation = Quaternion.Euler(randomDirecton);
-        transform.rotation = Quaternion.Lerp(transform.rotation, nextRotation, Random.Range(0.5f, 2.5f));
+        //transform.rotation = Quaternion.Lerp(transform.rotation, nextRotation, Random.Range(0.5f, 2.5f));
         chooseDirection = false;
     }
 
