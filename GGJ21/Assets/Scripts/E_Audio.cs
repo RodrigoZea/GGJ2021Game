22b28ed6626076418176cf792e2e80d7817a55cc
josepablito16 <josepablocif16@gmail.com>
2021-01-31 13:03:37 -0600

    FX and game music

---
+++ GGJ21/Assets/Scripts/E_Audio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_Audio : MonoBehaviour
+{
+    public AudioSource gameOver;
+    public AudioSource gameTheme;
+    public AudioSource doorsOpen;
+    public AudioSource gameWin;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void PlayGameWin()
+    {
+        gameWin.Play();
+    }
+    public void PlayGameOver()
+    {
+        gameOver.Play();
+    }
+
+    public void PlayDoorsOpen()
+    {
+        doorsOpen.Play();
+    }
+
+    public void PlayGameTheme()
+    {
+        gameTheme.Play();
+    }
+
+
+}
